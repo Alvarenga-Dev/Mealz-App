c2e4faea11ceb11ec17adeac7d8563930d3afd32
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2023-07-01 17:08:16 -0300

    chore: setup project

---
+++ app/src/main/java/com/alvarengadev/mealzapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.alvarengadev.mealzapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alvarengadev.mealzapp.ui.theme.MealzAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MealzAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen("World")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MealzAppTheme {
+        MainScreen("Android")
+    }
+}
