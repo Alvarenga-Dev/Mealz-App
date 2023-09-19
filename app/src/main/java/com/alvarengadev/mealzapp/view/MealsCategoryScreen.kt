0ed43cbdfe74fbe75e2cae308619cbf97af3a62a
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2023-09-19 12:11:54 -0300

    feature: create animation with compose

--- app/src/main/java/com/alvarengadev/mealzapp/view/MealsCategoryScreen.kt
+++ app/src/main/java/com/alvarengadev/mealzapp/view/MealsCategoryScreen.kt
@@ -1,5 +1,7 @@
 package com.alvarengadev.mealzapp.view
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -9,14 +11,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -41,6 +52,8 @@ fun MealsCategoriesScreen() {
 
 @Composable
 fun MealCategory(meal: MealResponse) {
+    var isExpanded by remember { mutableStateOf(false) } // create interaction, is a necessary use remember!!
+    var lines by remember { mutableStateOf(0) }
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(2.dp), // use CardDefaults for create elevation in Card.
@@ -48,7 +61,7 @@ fun MealCategory(meal: MealResponse) {
             .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
-        Row {
+        Row (modifier = Modifier.animateContentSize()) {
             SubcomposeAsyncImage(
                 model = meal.imageUrl,
                 loading = {
@@ -63,13 +76,36 @@ fun MealCategory(meal: MealResponse) {
             Column(
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
+                    .fillMaxWidth(.8f) // Is a necessary space
                     .padding(16.dp)
             ) {
                 Text(
                     text = meal.name,
                     style = MaterialTheme.typography.headlineSmall
                 )
+                Text(
+                    text = meal.description,
+                    style = MaterialTheme.typography.labelSmall,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = if (isExpanded) 15 else 4
+                )
             }
+            Icon(
+                imageVector = if (isExpanded)
+                    Icons.Filled.KeyboardArrowDown
+                else
+                    Icons.Filled.KeyboardArrowUp,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(
+                        if (isExpanded)
+                            Alignment.Bottom
+                        else
+                            Alignment.CenterVertically
+                    )
+                    .clickable { isExpanded = !isExpanded }
+            )
         }
     }
 }
