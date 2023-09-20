aeb813adeb9119316f8864a197d2903f6b629fb9
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2023-09-20 19:47:51 -0300

    feat: create dynamic multiple animations

--- app/src/main/java/com/alvarengadev/mealzapp/view/MealDetailsScreen.kt
+++ app/src/main/java/com/alvarengadev/mealzapp/view/MealDetailsScreen.kt
@@ -1,11 +1,14 @@
 package com.alvarengadev.mealzapp.view
 
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.gestures.transformable
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
@@ -17,50 +20,60 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
-import coil.transform.CircleCropTransformation
 import com.alvarengadev.mealzapp.data.resposes.MealResponse
 
 @Composable
 fun MealDetailsScreen(meal: MealResponse?) {
-    var isExpanded by remember { mutableStateOf(false) } // create interaction, is a necessary use remember!!
-    val imageSizeDp: Dp by animateDpAsState(
-        targetValue = if (isExpanded) 200.dp else 100.dp
-    )
-    Column(modifier = Modifier.padding(16.dp)) {
+    var profilePictureState by remember { mutableStateOf(MealProfilePictureState.Normal) }
+    val transition = updateTransition(targetState = profilePictureState, label = "")
+    val imageSizeDp by transition.animateDp(targetValueByState = { it.size }, label = "")
+    val color by transition.animateColor(targetValueByState = { it.color }, label = "")
+    val widthSize by transition.animateDp(targetValueByState = { it.borderWidth }, label = "")
+
+    Column {
         Row {
-            SubcomposeAsyncImage(
-                model = meal?.imageUrl,
-                loading = {
-                    CircularProgressIndicator()
-                },
-                contentDescription = null,
-                modifier = Modifier
-                    .size(imageSizeDp)
-            )
+            Card(
+                modifier = Modifier.padding(16.dp),
+                shape = CircleShape,
+                border = BorderStroke(
+                    width = widthSize,
+                    color = color
+                )
+            ) {
+                SubcomposeAsyncImage(
+                    model = meal?.imageUrl,
+                    loading = {
+                        CircularProgressIndicator()
+                    },
+                    contentDescription = null,
+                    modifier = Modifier.size(imageSizeDp)
+                )
+            }
             Text(
-                text = meal?.name ?: "Default name",
+                text = meal?.name ?: "default name",
                 modifier = Modifier
-                    .padding(8.dp)
-                    .align(Alignment.CenterVertically),
-                textAlign = TextAlign.Center
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically)
             )
         }
-        Button(
-            modifier = Modifier.padding(16.dp),
-            onClick = { isExpanded = !isExpanded }
-        ) {
-            Text(text = "Change state of meal profile picture")
+        Button(modifier = Modifier
+            .padding(16.dp),
+            onClick = {
+                profilePictureState = if (profilePictureState == MealProfilePictureState.Normal)
+                    MealProfilePictureState.Expanded
+                else
+                    MealProfilePictureState.Normal
+            }) {
+            Text("Change state of meal profile picture")
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun PreviewMealDetailsScreen() {
-    MealDetailsScreen(meal = null)
+enum class MealProfilePictureState(val color: Color, val size: Dp, val borderWidth: Dp) {
+    Normal(Color.Magenta, 120.dp, 8.dp),
+    Expanded(Color.Green, 200.dp, 24.dp)
 }
